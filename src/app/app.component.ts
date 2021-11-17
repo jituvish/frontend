ec59e2d6fa8bbb97ed3bc37255f55b23fbae1615
jituvish <jitendrakarma895@gmail.com>
2021-11-17 23:15:33 +0530

    image upload in angular8 nodejs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProfileService } from './services/profile.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  state = [];
+  city = [];
+  constructor(private _proServ : ProfileService) {
+    this.state = this._proServ.state();
+
+    
+  }
+
+
+  onChange(state){
+    this.city = this._proServ.city().filter(e=>e.id == state.target.value);
+    
+  }
+
+
+
+
   title = 'frontend';
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllProfilesComponent } from './component/all-profiles/all-profiles.component';
+import { CreatProfileComponent } from './component/creat-profile/creat-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllProfilesComponent,
+    CreatProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
