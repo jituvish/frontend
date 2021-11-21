288183fc7e9363c5075a30c9d428a6ede5614789
jituvish <jitendrakarma895@gmail.com>
2021-11-21 16:57:32 +0530

    modify in two file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllProfilesComponent } from './component/all-profiles/all-profiles.component';
 import { CreatProfileComponent } from './component/creat-profile/creat-profile.component';
+import { DropdownComponent } from './component/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AllProfilesComponent,
-    CreatProfileComponent
+    CreatProfileComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
